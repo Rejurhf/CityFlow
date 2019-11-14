26fba9065ecc7f74cb623bdcf6c297f8dd8f6da2
Rejurhf <mziomek108@gmail.com>
2019-11-14 22:17:49 +0100

    View layers ploting in progress

--- Utilities/airflow.py
+++ Utilities/airflow.py
@@ -1,4 +1,6 @@
 import numpy as np
+from Utilities import visualize, rays2dcalculator
+
 
 class AirFlow:
   def __init__(self, xSize, ySize, zSize, densPerMeter, obstacleList):
@@ -14,26 +16,57 @@ class AirFlow:
     nz = int(self.zSize * self.densPerMeter) + 1
 
     # Create 3d arrays
-    self.vX = np.zeros((self.zSize, self.ySize, self.xSize))
-    self.vY = np.zeros((self.zSize, self.ySize, self.xSize))
-    self.vZ = np.zeros((self.zSize, self.ySize, self.xSize))
-    self.p = np.zeros((self.zSize, self.ySize, self.xSize))
+    self.vX = np.zeros((nz, ny, nx))
+    self.vY = np.zeros((nz, ny, nx))
+    self.vZ = np.zeros((nz, ny, nx))
+    self.p = np.zeros((nz, ny, nx))
+
+
+  def convertObstaclesTo2d(self, isTopView = True):
+    obstacleList2d = []
+    if isTopView:
+      for obstacle in self.obstacleList:
+        obstacleList2d.append([obstacle[0], obstacle[1], obstacle[2], obstacle[3]])
+    else:
+        obstacleList2d.append([obstacle[0], obstacle[1], obstacle[4], obstacle[5]])
+    
+    return obstacleList2d
 
 
   # Calculate flow -----------------------------------------------------------------------
   def calculateFlow(self):
-    print("Flow calculated")
+    print("AF:", "Flow calculated")
 
 
   # Get top view for specified meter above ground
   def getTopViewLayerForMeter(self, meterAboveGround):
+    # Calculate layer coresponding to given meter
     zLayer = int(meterAboveGround * self.densPerMeter)
 
-    print("TODO Top view - {}m coresponds to {} z layer".format(meterAboveGround, zLayer))
+    # Calculate values for plot
+    nx = int(self.xSize * self.densPerMeter) + 1 # number of points in grid
+    ny = int(self.ySize * self.densPerMeter) + 1
+
+    x = np.linspace(0, self.xSize, nx) # last point included, so exactly nx points
+    y = np.linspace(0, self.ySize, ny) # last point included, so exactly ny points
+    
+    X,Y = np.meshgrid(x, y)     # makes 2-dimensional mesh grid
+    array2dX = self.vX[zLayer]  # Get layers
+    array2dY = self.vY[zLayer]
+    array2dP = self.p[zLayer]
+    listOf2dObstacles = self.convertObstaclesTo2d(isTopView = True)
+
+    # Print text and display plot
+    titleText = "{}m above ground ({} Z axis layer)".format(meterAboveGround, zLayer+1)
+    print("AF:", titleText)
+    visualize.showPlot(X, Y, array2dX, array2dY, array2dP, listOf2dObstacles, titleText)
 
   
   # Get top view for specified meter above ground
   def getSideViewLayerForMeter(self, meterFromY0):
+    # Calculate layer coresponding to given meter
     yLayer = int(meterFromY0 * self.densPerMeter)
     
-    print("TODO Side view - {}m coresponds to {} y layer".format(meterFromY0, yLayer))
+    # Print text and display plot
+    titleText = "{}m from left ({} Y axis layer)".format(meterFromY0, yLayer+1)
+    print("AF:", titleText)
--- flowforcuboid.py
+++ flowforcuboid.py
@@ -1,6 +1,6 @@
 from Utilities import visualize, airflow
 
-print("Start simulation")
+print("FFC:", "Start simulation")
 
 xSize = 30  # nuberic size of in eg metres
 ySize = 20
@@ -8,7 +8,7 @@ zSize = 10
 densPerMeter = 2.5 # points density per 1 meter
 
 # Draw in xSize, ySize, zSize [xStart, xDepth, yStart, yDepth, zStart, zDepth]
-obstacleList = [[6, 3, 8, 3, 0, 4]]
+obstacleList = [[6, 3, 8, 4, 0, 4]]
 
 # Create case object
 testFlow = airflow.AirFlow(xSize, ySize, zSize, densPerMeter, obstacleList)
@@ -17,7 +17,7 @@ testFlow = airflow.AirFlow(xSize, ySize, zSize, densPerMeter, obstacleList)
 testFlow.calculateFlow()
 
 # Get top view for 2m above ground
-testFlow.getTopViewLayerForMeter(10)
+testFlow.getTopViewLayerForMeter(2)
 
 # Get side view for 10m from left
-testFlow.getSideViewLayerForMeter(0)
+testFlow.getSideViewLayerForMeter(10)
