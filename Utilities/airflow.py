3a0820ff05016a4d244f5f2ab89e324416364a53
Rejurhf <mziomek108@gmail.com>
2019-11-14 22:42:57 +0100

    AirFlow top and side visualisation done

--- Utilities/airflow.py
+++ Utilities/airflow.py
@@ -21,16 +21,34 @@ class AirFlow:
     self.vZ = np.zeros((nz, ny, nx))
     self.p = np.zeros((nz, ny, nx))
 
-
+  # Convert 3d obstacles to 2d
   def convertObstaclesTo2d(self, isTopView = True):
     obstacleList2d = []
     if isTopView:
       for obstacle in self.obstacleList:
         obstacleList2d.append([obstacle[0], obstacle[1], obstacle[2], obstacle[3]])
     else:
+      for obstacle in self.obstacleList:
         obstacleList2d.append([obstacle[0], obstacle[1], obstacle[4], obstacle[5]])
     
     return obstacleList2d
+
+
+  # Get side view from 3d array
+  def getSideView2dArray(self, array3d, yLayer):
+    # Calculate points in every axis
+    nx = int(self.xSize * self.densPerMeter) + 1 # number of points in grid
+    nz = int(self.zSize * self.densPerMeter) + 1
+
+    # Declare empty array to return
+    array2d = np.empty([nz, nx])
+
+    # Convert 3d array to 2d
+    for i in range(nz):
+      for j in range(nx):
+        array2d[i,j] = array3d[i, yLayer, j]
+      
+    return array2d
 
 
   # Calculate flow -----------------------------------------------------------------------
@@ -38,7 +56,7 @@ class AirFlow:
     print("AF:", "Flow calculated")
 
 
-  # Get top view for specified meter above ground
+  # Get top view for specified meter above ground ----------------------------------------
   def getTopViewLayerForMeter(self, meterAboveGround):
     # Calculate layer coresponding to given meter
     zLayer = int(meterAboveGround * self.densPerMeter)
@@ -62,11 +80,25 @@ class AirFlow:
     visualize.showPlot(X, Y, array2dX, array2dY, array2dP, listOf2dObstacles, titleText)
 
   
-  # Get top view for specified meter above ground
+  # Get side view for specified meter above ground --------------------------------------- 
   def getSideViewLayerForMeter(self, meterFromY0):
     # Calculate layer coresponding to given meter
     yLayer = int(meterFromY0 * self.densPerMeter)
+
+    # Calculate values for plot
+    nx = int(self.xSize * self.densPerMeter) + 1 # number of points in grid
+    nz = int(self.zSize * self.densPerMeter) + 1
+
+    x = np.linspace(0, self.xSize, nx) # last point included, so exactly nx points
+    z = np.linspace(0, self.zSize, nz) # last point included, so exactly ny points
+    
+    X,Z = np.meshgrid(x, z)     # makes 2-dimensional mesh grid
+    array2dX = self.getSideView2dArray(self.vX, yLayer) # Get layers
+    array2dZ = self.getSideView2dArray(self.vZ, yLayer)
+    array2dP = self.getSideView2dArray(self.p, yLayer)
+    listOf2dObstacles = self.convertObstaclesTo2d(isTopView = False)
     
     # Print text and display plot
     titleText = "{}m from left ({} Y axis layer)".format(meterFromY0, yLayer+1)
     print("AF:", titleText)
+    visualize.showPlot(X, Z, array2dX, array2dZ, array2dP, listOf2dObstacles, titleText)
