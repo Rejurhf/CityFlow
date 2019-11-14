97ac3eed1377af92780dbec52c446ca4bba0fa82
Rejurhf <mziomek108@gmail.com>
2019-11-14 20:56:46 +0100

    Tab converted from 4 to 2 spaces

---
+++ Utilities/airflow.py
@@ -0,0 +1,9 @@
+
+
+class AirFlow:
+  def __init__(self, xSize, ySize, zSize, densPerMeter, obstacleList):
+    self.xSize = xSize
+    self.ySize = ySize
+    self.zSize = zSize
+    self.densPerMeter = densPerMeter
+    self.obstacleList = obstacleList
