9f9109d6cd23b3faeae42065dfe72a176ea2392d
Rejurhf <mziomek108@gmail.com>
2019-11-14 21:35:30 +0100

    AirFlow and flowforcuboid init

--- Utilities/airflow.py
+++ Utilities/airflow.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 class AirFlow:
   def __init__(self, xSize, ySize, zSize, densPerMeter, obstacleList):
@@ -7,3 +7,33 @@ class AirFlow:
     self.zSize = zSize
     self.densPerMeter = densPerMeter
     self.obstacleList = obstacleList
+    
+    # Calculate points in every axis
+    nx = int(self.xSize * self.densPerMeter) + 1 # number of points in grid
+    ny = int(self.ySize * self.densPerMeter) + 1
+    nz = int(self.zSize * self.densPerMeter) + 1
+
+    # Create 3d arrays
+    self.vX = np.zeros((self.zSize, self.ySize, self.xSize))
+    self.vY = np.zeros((self.zSize, self.ySize, self.xSize))
+    self.vZ = np.zeros((self.zSize, self.ySize, self.xSize))
+    self.p = np.zeros((self.zSize, self.ySize, self.xSize))
+
+
+  # Calculate flow -----------------------------------------------------------------------
+  def calculateFlow(self):
+    print("Flow calculated")
+
+
+  # Get top view for specified meter above ground
+  def getTopViewLayerForMeter(self, meterAboveGround):
+    zLayer = int(meterAboveGround * self.densPerMeter)
+
+    print("TODO Top view - {}m coresponds to {} z layer".format(meterAboveGround, zLayer))
+
+  
+  # Get top view for specified meter above ground
+  def getSideViewLayerForMeter(self, meterFromY0):
+    yLayer = int(meterFromY0 * self.densPerMeter)
+    
+    print("TODO Side view - {}m coresponds to {} y layer".format(meterFromY0, yLayer))
---
+++ flowforcuboid.py
@@ -0,0 +1,23 @@
+from Utilities import visualize, airflow
+
+print("Start simulation")
+
+xSize = 30  # nuberic size of in eg metres
+ySize = 20
+zSize = 10
+densPerMeter = 2.5 # points density per 1 meter
+
+# Draw in xSize, ySize, zSize [xStart, xDepth, yStart, yDepth, zStart, zDepth]
+obstacleList = [[6, 3, 8, 3, 0, 4]]
+
+# Create case object
+testFlow = airflow.AirFlow(xSize, ySize, zSize, densPerMeter, obstacleList)
+
+# Make simulation
+testFlow.calculateFlow()
+
+# Get top view for 2m above ground
+testFlow.getTopViewLayerForMeter(10)
+
+# Get side view for 10m from left
+testFlow.getSideViewLayerForMeter(0)
