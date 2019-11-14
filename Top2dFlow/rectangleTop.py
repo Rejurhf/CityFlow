b23d6653662c9d5b03c7aa6f42baeaabf8021d61
Rejurhf <mziomek108@gmail.com>
2019-11-14 18:52:22 +0100

    Unnecessery comments deleted

--- Top2dFlow/rectangleTop.py
+++ Top2dFlow/rectangleTop.py
@@ -12,14 +12,5 @@ obstacles = [[5,1.5,4.1,3.9], [13, 2, 2.6, 2]]
 X, Y, u, v, p = topRays.getFlowPathTopArrays(xSize, ySize, densPerMeter, 
     obstacles)
 
-
-
-# # u, v = self.getFlowPath(u, v, xSize, ySize, obstacle)
-# # u, v = self.calcInObstacleFlow(u, v, xSize, ySize, obstacle)
-# # u, v = self.calcUDecrease(u, v, xSize, ySize, obstacle)
-
-
-# showPlot(X, Y, u, v, p, obstacle, "Title")
-
 visualize.showPlot(X, Y, u, v, p, obstacles, "Test")
 print("Top Rays Done")
