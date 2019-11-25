482e87f948da71095d7f90db0d76140dfa8b7ae5
Rejurhf <mziomek108@gmail.com>
2019-11-25 20:27:03 +0100

    Integrated with air flow

--- flowFromOMSFile.py
+++ flowFromOMSFile.py
@@ -1,10 +1,31 @@
-from utilities import gridcreator
+from utilities import airflow, gridcreator
 
 print("FFOMS:", "Read OSM file")
 
+# initialize grid creator
 gridCreator = gridcreator.GridCreator("resources/map2.osm")
 
+# get grid elements
 xSize, ySize, zSize = gridCreator.getGridElements()
+densPerMeter = 1
+obstacleList = [[6, 3, 8, 4, 0, 4]]
 
 print("FFOMS:", "xSize: {}, ySize: {}, zSize: {}".format(xSize, ySize, zSize))
+print("FFOMS:", "Density per meter:", densPerMeter)
 print("FFOMS:", "Buildings: {}".format(gridCreator.buildingListSize()))
+
+flow = airflow.AirFlow(xSize, ySize, zSize, densPerMeter, obstacleList)
+
+# Make simulation
+print("FFC:", "Start simulation")
+flow.calculateFlow()
+
+# Get top view for 2m above ground
+print("FFC:", "Show Top View")
+flow.getTopViewLayerForMeter(2)
+
+# Get side view for 10m from left
+print("FFC:", "Show Side View")
+flow.getSideViewLayerForMeter(10)
+
+
